ca8f045ed2d39dea0e948747a3b45ede1c6b7ff1
Héctor Ruiz <98161643+IT-Noobie@users.noreply.github.com>
2022-05-24 01:38:54 +0200

    Update sshScript.sh

--- sshScript.sh
+++ sshScript.sh
@@ -12,22 +12,30 @@ godPassword=$2
 username=$3
 password=$4
 
-# Create users folders
-mkdir /home/noprv/${username}
-mkdir /home/noprv/${username}/certs
-
 # Create user specifying directory, group and shell
-echo ${godPassword} | sudo -S useradd -d /home/noprv/${username} -s /bin/bash -G noprv-users $username
+echo ${godPassword} | sudo -S useradd -d /home/${username} -s /bin/rbash -G zeus -m  $username
+mkdir /home/${username}/certs
 
 # Creates user certificates and context for Kubernetes
-cd /home/noprv/${username}
+cd /home/${username}
 
+# Generates rolebinding and certificates. Configures credentials and sets it up
 kubectl create rolebinding noprv_${RANDOM} --role noprv-role --user ${username} -n application
 openssl genrsa -out ${username}.key 2048
 openssl req -new -key ${username}.key -out ${username}.csr -subj "/CN=${username}/O=user"
 openssl x509 -req -in ${username}.csr -CA /home/zeus/certs/ca.crt -CAkey /home/zeus/certs/ca.key -CAcreateserial -out ${username}.crt -days 10000
 kubectl config set-credentials ${username} --client-certificate=./${username}.crt --client-key=./${username}.key
-kubectl config set-context ${username}-context --cluster=kubernetes --namespace=default --user=${username}
+kubectl config set-context ${username}-context --cluster=kubernetes --namespace=application --user=${username}
+
 
 # Copies all the templates to username folder
-cp -r /home/zeus/templates /home/noprv/${username}/
+cp -r /home/zeus/scripts/userDeploy.sh /home/${username}
+cp -r /home/zeus/templates /home/${username}/
+cp -r /home/zeus/bin /home/${username}/
+echo ${godPassword} | sudo -S chown -R ${username}:zeus /home/${username}
+
+echo ${godPassword} | sudo -S bash -c 'cat <<EOF >> /home/alicia/.bashrc
+alias kubectl="kubectl --context=alicia-context"
+PATH=/home/alicia/bin
+export PATH
+EOF'
