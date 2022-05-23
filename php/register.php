5a5aa58cde36e8ee1baf4adb15e861da6bff24bc
Héctor Ruiz <98161643+IT-Noobie@users.noreply.github.com>
2022-05-23 08:37:15 +0200

    Update register.php

--- php/register.php
+++ php/register.php
@@ -1,14 +1,16 @@
 <?php
 include_once 'configInsertRole.php';
 session_start();
+
 # RECEIVE VALUES FROM LOGIN
 $username = $_POST['username'];
+$username .= "_user";
 $email = $_POST['email'];
-$contra = password_hash($_POST['password'], PASSWORD_DEFAULT);
+$contra = $_POST['password'];
+$contraHashed = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
 $_SESSION['username'] = $username;
 $_SESSION['email'] = $email;
-$_SESSION['contra'] = $contra;
 
 if ( empty($username) || empty($email) || empty($contra)) {
     if(empty($username)){
@@ -22,8 +24,15 @@ if ( empty($username) || empty($email) || empty($contra)) {
     }
 } else {
     # INSERT VALUES INTO DATABASE
-    $insertar = mysqli_query($conexion, "INSERT INTO User VALUES ('$username', '$email', '$contra')");
-	header("Location: ../images.php");
-	}
-	
+    $insertar = mysqli_query($conexion, "INSERT INTO User VALUES ('$username', '$email', '$contraHashed')");
+    $sshConnection = ssh2_connect('IP_CONTROLPLANE', 22);
+    if (ssh2_auth_password($sshConnection, 'zeus', 'zeus')) {
+        #echo "authentication Successful\n";
+        $stream = ssh2_exec($sshConnection, "bash /home/zeus/scripts/sshScript.sh zeus '$username' '$contra'");
+        #echo "hecho";
+        #header("Location: ../images.php");
+    } else {
+        die('Authentication Failed');
+    }
+       }
 ?>
