1fd28f12cb549faa815f6b499c849c84bf6c8da4
Héctor Ruiz Aceijas <ruac.hector@gmail.com>
2022-05-24 18:22:05 +0200

    Pre-Final commit

--- php/logout.php
+++
@@ -1,7 +0,0 @@
- <?php
-	include_once "configDeleteRole.php";
-	session_start();
-	$username = $_SESSION['username'];
-	$eliminar = mysqli_query($conexion, "DELETE FROM User WHERE username='$username'");
-	unset($_SESSION["username"]);
-?>
