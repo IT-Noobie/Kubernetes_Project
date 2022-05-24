7712cc94d2ade87d1c2090c9624f01c2a5512cd1
Héctor Ruiz <98161643+IT-Noobie@users.noreply.github.com>
2022-05-24 02:24:18 +0200

    Update afterInstall.sh

--- afterInstall.sh
+++ afterInstall.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 kubectl create namespace prometheus
 helm install prometheus prometheus-community/kube-prometheus-stack --namespace=prometheus
-kubectl create namespace application
+kubectl label node k8s-node-1 node-role.kubernetes.io/worker=worker
