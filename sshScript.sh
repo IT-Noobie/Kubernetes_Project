ac4959700bfe959f33e0b7a00fd1ac83589bd0f5
Héctor Ruiz <98161643+IT-Noobie@users.noreply.github.com>
2022-05-24 17:24:00 +0200

    Update sshScript.sh

--- sshScript.sh
+++ sshScript.sh
@@ -1,5 +1,3 @@
-#/bin/bash
-
 if [ $# -ne 3 ]
 then
     echo 'Arguments parsing error'
@@ -7,35 +5,43 @@ then
 fi
 
 # Stores username and password received by ssh connection
-godPassword=$2
-
-username=$3
-password=$4
+godPassword=$1
+username=$2
+password=$3
 
 # Create user specifying directory, group and shell
-echo ${godPassword} | sudo -S useradd -d /home/${username} -s /bin/rbash -G zeus -m  $username
+echo ${godPassword} | sudo -S useradd -d /home/${username} -s /bin/rbash -m ${username}
+echo ${godPassword} | sudo -S chown -R ${username}:zeus /home/${username}
+echo ${godPassword} | sudo -S chmod 770 /home/${username}
 mkdir /home/${username}/certs
 
 # Creates user certificates and context for Kubernetes
 cd /home/${username}
-
 # Generates rolebinding and certificates. Configures credentials and sets it up
+cp -r /home/zeus/templates /home/${username}/
+
 kubectl create rolebinding noprv_${RANDOM} --role noprv-role --user ${username} -n application
 openssl genrsa -out ${username}.key 2048
 openssl req -new -key ${username}.key -out ${username}.csr -subj "/CN=${username}/O=user"
 openssl x509 -req -in ${username}.csr -CA /home/zeus/certs/ca.crt -CAkey /home/zeus/certs/ca.key -CAcreateserial -out ${username}.crt -days 10000
 kubectl config set-credentials ${username} --client-certificate=./${username}.crt --client-key=./${username}.key
 kubectl config set-context ${username}-context --cluster=kubernetes --namespace=application --user=${username}
+kubectl config use-contest ${username}-context
 
+# Deletes kubernetes-admin from user file kube/config 
+yq 'del(.users.[0] | select(.name == "kubernetes-admin"))' templates/.kube/config > templates/.kube/config.1
+yq 'del(.contexts.[0].context | select(.user == "kubernetes-admin"))' templates/.kube/config.1 > templates/.kube/config.2
+yq 'del(.contexts.[0] | select(.name == "kubernetes-admin@kubernetes"))' templates/.kube/config.2 > templates/.kube/config
+mv /home/zeus/templates/kube /home/${username}/.kube
 
 # Copies all the templates to username folder
-cp -r /home/zeus/scripts/userDeploy.sh /home/${username}
-cp -r /home/zeus/templates /home/${username}/
+cp /home/zeus/scripts/userDeploy.sh /home/${username}
 cp -r /home/zeus/bin /home/${username}/
-echo ${godPassword} | sudo -S chown -R ${username}:zeus /home/${username}
 
-echo ${godPassword} | sudo -S bash -c 'cat <<EOF >> /home/alicia/.bashrc
-alias kubectl="kubectl --context=alicia-context"
-PATH=/home/alicia/bin
+sudo bash -c 'cat <<EOF >> /home/${username}/.bashrc
+alias kubectl="kubectl --context=${username}-context"
+PATH=/home/${username}/bin
 export PATH
 EOF'
+
+echo ${godPassword} | sudo -S chown -R ${username}:zeus /home/${username}
